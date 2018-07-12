e0e20d64ebfeb7368f96cb674754c93dc2ab4a91
mjtabaya <mjtabaya@gmail.com>
2018-07-13 02:51:59 +0800

    Fix migrations

--- db/migrate/20180712180042_import_weapons_csv.rb
+++ db/migrate/20180712180042_import_weapons_csv.rb
@@ -1,11 +1,14 @@
 class ImportWeaponsCsv < ActiveRecord::Migration[5.1]
-  def change
+  def up
     require 'csv'
-
       csv_text = File.read('db/weapons.csv').force_encoding("UTF-8")
       csv = CSV.parse(csv_text, :headers => true)
       csv.each do |row|
         Weapon.create!(row.to_hash)
-      end
+    end
+  end
+
+  def down
+    Weapon.delete_all
   end
 end
---
+++ db/migrate/20180712184131_import_users_captains_skyfarers_csv.rb
@@ -0,0 +1,33 @@
+class ImportUsersCaptainsSkyfarersCsv < ActiveRecord::Migration[5.1]
+  def up
+    require 'csv'
+
+      csv_text = File.read('db/users.csv').force_encoding("UTF-8")
+      csv = CSV.parse(csv_text, :headers => true)
+      csv.each do |row|
+        User.create!(row.to_hash)
+    end
+
+    require 'csv'
+
+      csv_text = File.read('db/captains.csv').force_encoding("UTF-8")
+      csv = CSV.parse(csv_text, :headers => true)
+      csv.each do |row|
+        Captain.create!(row.to_hash)
+    end
+
+    require 'csv'
+
+      csv_text = File.read('db/skyfarers.csv').force_encoding("UTF-8")
+      csv = CSV.parse(csv_text, :headers => true)
+      csv.each do |row|
+        Skyfarer.create!(row.to_hash)
+    end
+  end
+
+  def down
+    Skyfarer.delete_all
+    Captain.delete_all
+    User.delete_all
+  end
+end
