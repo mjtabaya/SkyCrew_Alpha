91844efdabb60a3919d61de5ae32a97d48701341
mjtabaya <mjtabaya@gmail.com>
2018-07-13 02:14:33 +0800

    Weapons CSV Import

--- db/migrate/20180712180042_import_weapons_csv.rb
+++ db/migrate/20180712180042_import_weapons_csv.rb
@@ -1,4 +1,11 @@
 class ImportWeaponsCsv < ActiveRecord::Migration[5.1]
   def change
+    require 'csv'
+
+      csv_text = File.read('db/weapons.csv').force_encoding("UTF-8")
+      csv = CSV.parse(csv_text, :headers => true)
+      csv.each do |row|
+        Weapon.create!(row.to_hash)
+      end
   end
 end
