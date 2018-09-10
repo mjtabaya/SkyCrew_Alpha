504954519d9b3869d2791e0a8fcbbdb88b60a616
mjtabaya <mjtabaya@gmail.com>
2018-09-10 11:35:11 +0800

    Refactor to follow ruby style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,24 +56,22 @@ class UsersController < ApplicationController
 
   private
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    set_user
+    unless current_user?(@user) || @current_user.username == "unoone"
+      flash[:danger] = "Unable to access that."
+      redirect_to current_user
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation)
-    end
-
-    # Confirms the correct user.
-    def correct_user
-      set_user
-      unless current_user?(@user) || @current_user.username == "unoone"
-        flash[:danger] = "Unable to access that."
-        redirect_to current_user
-      end
-    end
-
-
+  end
 end
